30152940216bb5e4f0592d69f7f4dae2a79661e2
liaowenming23 <liaowenming23@gmail.com>
2018-06-19 22:35:50 +0800

    Initial check-in of module Leetcode

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Leetcode
+{
+    class MainClass
+    {
+        public static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //No.771
+        public int NumJewelsInStones(string J, string S)
+        {
+            int num = 0;
+            int J_len = J.Length;
+            int S_len = S.Length;
+            for (int i = 0; i < S_len; i++){
+                for (int j = 0; j < J_len;j++){
+                    if(S[i].Equals(J[j])){
+                        num++;
+                        break;
+                    }
+                } 
+            }
+            return num;
+        }
+    }
+}
